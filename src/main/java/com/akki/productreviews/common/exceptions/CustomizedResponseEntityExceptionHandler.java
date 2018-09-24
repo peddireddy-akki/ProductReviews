fed2106194e40d1427272b007a3aafcff5afb816
peddireddy-akki <peddireddy.akki@gmail.com>
2018-09-24 18:57:45 +0530

    Final

--- src/main/java/com/akki/productreviews/common/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/akki/productreviews/common/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -8,8 +8,11 @@
 
 package com.akki.productreviews.common.exceptions;
 
+import java.time.LocalDateTime;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,15 +24,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 	private static final Logger logger = LogManager.getLogger(CustomizedResponseEntityExceptionHandler.class);
-	
+
 	@ExceptionHandler(ApplicationException.class)
-	public final ResponseEntity<ExceptionResponse> handleUserNotFoundException(ApplicationException appException,
+	public final ResponseEntity<ExceptionResponse> handleApplicationException(ApplicationException appException,
 			WebRequest request) {
 		logger.error(appException, appException);
 		ExceptionResponse exceptionResponse = new ExceptionResponse(appException.getExceptionTimeStamp(),
 				appException.getErrorCode(), appException.getMessage(), appException.getErrorDetails(),
 				request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, appException.getHttpStatus());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleException(ApplicationException exception, WebRequest request) {
+		logger.error(exception, exception);
+		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), "9999", "Undled Exception",
+				"Internal server error. Please contact customer service team", request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
