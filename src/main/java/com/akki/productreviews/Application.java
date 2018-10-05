e541f47ad71a87ea786815a30a1c7f5e23b49c26
peddireddy-akki <peddireddy.akki@gmail.com>
2018-10-05 15:49:05 +0530

    Log cleaned
    Log cleaned

--- src/main/java/com/akki/productreviews/Application.java
+++ src/main/java/com/akki/productreviews/Application.java
@@ -10,17 +10,15 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
 
 	private static final Logger LOGGER = LogManager.getLogger(Application.class);
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        
-        System.out.println("\n\n\n ProductReviewApplication Started Successfull");
-        
-        LOGGER.debug("Log4j issue");
 
-    }   
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+		System.out.println("\n\n\n ProductReviewApplication Started Successfull");
+
+	}
 }
