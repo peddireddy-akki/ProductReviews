6cff916491ccdf0981acd654f9c658c726716bc4
peddireddy-akki <peddireddy.akki@gmail.com>
2018-10-06 13:14:55 +0530

    removed logger

--- src/main/java/com/akki/productreviews/Application.java
+++ src/main/java/com/akki/productreviews/Application.java
@@ -17,8 +17,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	private static final Logger LOGGER = LogManager.getLogger(Application.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
